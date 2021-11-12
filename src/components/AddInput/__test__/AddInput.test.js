a427eb89fcaadc715098fa06d78320adf11cb89c
U.A. Yanık <atesyanik@gmail.com>
2021-11-12 23:51:12 +0100

    input area test extras

--- src/components/AddInput/__test__/AddInput.test.js
+++ src/components/AddInput/__test__/AddInput.test.js
@@ -23,9 +23,11 @@ describe('Unit test for AddInput component', () => {
             />
         );
         const inputElement = screen.getByPlaceholderText(/Add a new task here.../i);
+        const buttonElement = screen.getByRole( 'button', {name: /Add/i })
         fireEvent.change(inputElement, {target: {value: 'Go Grocery Shopping'}})
+        fireEvent.click(buttonElement)
 
-        expect(inputElement.value).toBe('Go Grocery Shopping');
+        expect(inputElement.value).toBe('');
     });
 
     it('should have empty input wwhe', () => {
